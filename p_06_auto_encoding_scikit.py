3401c1276f0c0d22d50e4d744ea7c641b50f6b2d
Abigail Lu <abigailitliong.lu@calbaptist.edu>
2020-04-15 22:40:04 -0700

    created txt files for scikit vectors

--- p_06_auto_encoding_scikit.py
+++ p_06_auto_encoding_scikit.py
@@ -31,14 +31,20 @@ def scikit_fq(corpus_one):
     vectorizer = CountVectorizer()
     vectors_final_scikit = vectorizer.fit_transform(corpus_one)#Use default corpus yes
     print('These are the scikit vectors:')
-    print(vectors_final_scikit)
-    print()
+    ##print(vectors_final_scikit) ##don't need this anymore :)
+    ##print()
+    f = open('scikit_fq.txt', 'w+')
+    f.write(str(vectors_final_scikit))
+    f.close()
     #"""
 
 
 def scikit_one_hot(corpus_one):
     print('scikit_one_hot')
-    print(corpus_one)
+    ##print(corpus_one) ##don't need this anymore :)
+    f = open('scikit_one_hot.txt', 'w+')
+    f.write(str(corpus_one))
+    f.close()
     #corpus_one = nltk.sent_tokenize(corpus_one)
     # for x in corpus_one:
     #     print(x)
@@ -49,12 +55,21 @@ def scikit_one_hot(corpus_one):
     vectors_final_scikit_one_hot = CountVectorizer()
     vectors_final_scikit_one_hot.fit(corpus_one)
     print('-----------------------------------------------------')
-    print(vectors_final_scikit_one_hot.vocabulary_)
+    ##print(vectors_final_scikit_one_hot.vocabulary_) ## don't need this anymore :)
+    f = open('scikit_one_hot_vocabulary.txt', 'w+')
+    f.write(str(vectors_final_scikit_one_hot.vocabulary_))
+    f.close()
 
     # vector_one_hot = Binarizer().fit(corpus_final)
     vector_all = vectors_final_scikit_one_hot.transform(corpus_one)
-    print(vector_all)
-    print(vectors_final_scikit_one_hot.fit_transform(corpus_one).toarray())
+    ##print(vector_all) ## don't need this anymore :)
+    f = open('scikit_one_hot_vector_all.txt', 'w+')
+    f.write(str(vector_all))
+    f.close()
+    ##print(vectors_final_scikit_one_hot.fit_transform(corpus_one).toarray()) ## don't need this anymore :)
+    f = open('scikit_one_hot_vector_all_array.txt', 'w+')
+    f.write(str(vectors_final_scikit_one_hot.fit_transform(corpus_one).toarray()))
+    f.close()
     # print('-----------------------------------------------------')
     
     # corpus_final = vector_one_hot.fit_transform(corpus_one)
@@ -67,7 +82,10 @@ def scikit_tf_idf(corpus_one):
     print('scikit_tf_idf')
     tfidf = TfidfVectorizer()
     corpus = tfidf.fit_transform(corpus_one)
-    print(corpus)
+    ##print(corpus) ## don't need this anymore :)
+    f = open('scikit_tf_idf.txt', 'w+')
+    f.write(str(corpus))
+    f.close()
 
 
 
